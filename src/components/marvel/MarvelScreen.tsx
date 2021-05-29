7a0676035a7d8b6c6d9960a6075d67c23fc4101d
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-05-29 18:01:54 +0200

    Add MarvelScreen.tsx

--- src/components/marvel/MarvelScreen.tsx
+++ src/components/marvel/MarvelScreen.tsx
@@ -1,8 +1,12 @@
+import { HeroList } from '../heroes/HeroList';
 
 export const MarvelScreen = () => {
+    const MARVEL_COMICS = 'Marvel Comics';
     return (
         <div>
             <h1>Marvel screen</h1>
+            <hr />
+            <HeroList publisher={MARVEL_COMICS} />
         </div>
     )
 }
