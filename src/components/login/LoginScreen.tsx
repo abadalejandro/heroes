0fde3257d90cf742044a7d211d097cce01943af9
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-05-29 17:20:36 +0200

    Add LoginScreen.tsx

--- src/components/login/LoginScreen.tsx
+++ src/components/login/LoginScreen.tsx
@@ -1,8 +1,17 @@
 
-export const LoginScreen = () => {
+
+
+export const LoginScreen =  ({history}:any) => {
+    const handleClick = (_e:React.FormEvent<EventTarget>) => {
+    //    history.push('/'); 
+       history.replace('/');
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Loginscreen</h1>
+            <hr />
+            <button className="btn btn-primary" onClick={handleClick}>Login</button>
         </div>
     )
 }
