211cbde2079ba1051dae86f3d0bccf453537015d
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-06-08 17:36:29 +0200

    AuthReducer tested ok

--- src/test/auth/authReducer.test.js
+++ src/test/auth/authReducer.test.js
@@ -1,5 +1,23 @@
-describe('Tests in authReducer', ()=>{
-    test('should', () => {
-        expect(true).toBe(true);
+import { authReducer } from "../../auth/authReducer";
+
+describe('Tests in authReducer', () => {
+    test('Should return default state.', () => {
+        const state = authReducer({ logged: false }, {});
+        expect(state).toEqual({ logged: false });
+    });
+    test('Should autenticate and return the username.', () => {
+        const action = {
+            type: 'login',
+            payload: { name: 'Susana' }
+        }
+        const state = authReducer({ logged: false }, action);
+        expect(state).toEqual({ logged: true, name: 'Susana' });
+    });
+    test('Should delete the username and set logged to false.', () => {
+        const action = {
+            type: 'logout'            
+        }
+        const state = authReducer({ logged: true, name: 'Fermin' }, action);
+        expect(state).toEqual({ logged: false, name: null });
     });
 });
