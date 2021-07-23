5e59d41f89c92dcd11b9e4d7bc120d268cd04f7a
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-07-23 10:44:31 +0200

    fix: change Router by HashRouter

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { HashRouter ,BrowserRouter as Router, Switch } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "../routers/DashboardRoutes";
 import { PrivateRoute } from './PrivateRoute';
@@ -9,13 +9,13 @@ import { PublicRoute } from './PublicRoute';
 export const AppRouter = () => {
     const { user } = useContext<any>(AuthContext);
     return (
-        <Router>
+        <HashRouter>
             <div>
                 <Switch>
                     <PublicRoute path="/login" component={LoginScreen} isAuthenticated={user.logged} />
                     <PrivateRoute path="/" component={DashboardRoutes} isAuthenticated={user.logged} />
                 </Switch>
             </div>
-        </Router>
+        </HashRouter>
     );
 }
